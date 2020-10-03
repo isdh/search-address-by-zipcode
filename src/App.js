afe36bbb14218d1b4c8172b905ee9744293b478e
Tomo Yanagi <hideorseekyes+github@gmail.com>
2020-10-04 02:11:50 +0900

    layout landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./output.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="bg-black min-h-screen flex flex-col items-center text-center justify text-white text-6xl">
+        <h1 className="text-9xl text-red-300">〒 郵便番号検索</h1>
+        <br />
+        <p className="text-2xl">
+          郵便番号をハイフン無しで入力してしてください。
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br />
+        <span className='text-5xl'>
+        〒<input
+          id='zipcode'
+          className="text-5xl text-gray-700"
+          itemType="text"
+          autoFocus
+          placeholder="1230001"
+          maxLength="7"
+          minLength="7"
+          size='8'
+        />
+        </span>
       </header>
     </div>
   );
