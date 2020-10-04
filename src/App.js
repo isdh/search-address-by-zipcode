e18b860e7919e218582c59672f072c90c4cf3911
Tomo Yanagi <hideorseekyes+github@gmail.com>
2020-10-05 02:24:47 +0900

    fix message

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Zipcode from "./Zipcode";
 
 function App() {
   return (
-    <div className="App bg-black min-h-screen flex flex-col items-center text-center justify text-white text-6xl">
+    <div className="bg-black min-h-screen flex flex-col items-center text-center justify text-white text-6xl">
       <header>
         <h1 className="text-9xl text-red-300">〒 郵便番号検索</h1>
         <br />
         <p className="text-2xl">
-          郵便番号をハイフン無しで入力してしてください。
+          郵便番号を半角数字(ハイフン無し)で入力してしてください。
         </p>
       </header>
       <section>
